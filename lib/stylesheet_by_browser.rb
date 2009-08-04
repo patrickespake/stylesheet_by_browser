0b52b46e1561430982fd986fa0aa4bdf86e10512
Patrick Espake <patrickespake@gmail.com>
2009-08-03 22:09:57 -0300

    add source comments

--- lib/stylesheet_by_browser.rb
+++ lib/stylesheet_by_browser.rb
@@ -1,6 +1,20 @@
+# Detects user's browser.
+#
+# Author:: Patrick Espake (mailto:patrickespake@gmail.com)
+# Copyright:: Copyright (c) 2009
+# License:: MIT
+
+# This module detects user's browser.
+#
+# Provides one method:
+#   user_agent_name
 module StylesheetByBrowser
   private
-  
+
+  # Name of the user's browser
+  #
+  # Example of use:
+  #   user_agent_name
   def user_agent_name
     ua = request.user_agent.downcase
 
--- lib/stylesheet_by_browser_helper.rb
+++ lib/stylesheet_by_browser_helper.rb
@@ -1,8 +1,25 @@
-require 'stylesheet_by_browser'
+# Helper insert the stylesheet according to the browser.
+#
+# Can be used in the layer view.
+#
+# Author:: Patrick Espake (mailto:patrickespake@gmail.com)
+# Copyright:: Copyright (c) 2009
+# License:: MIT
 
+# This module insert the stylesheet according to the browser.
+# Provides one method:
+#   stylesheet_by_browser
+require 'stylesheet_by_browser'
 module StylesheetByBrowserHelper
   include StylesheetByBrowser
 
+  # Helper insert the stylesheet according to the browser.
+  #
+  # Example of use:
+  #   <%= stylesheet_by_browser %>
+  #   <%= stylesheet_by_browser, :media => "all" %>
+  #   <%= stylesheet_by_browser, :media => "print" %>
+  #   <%= stylesheet_by_browser, :media => "screen" %>
   def stylesheet_by_browser(options = {})
     unless user_agent_name.nil?
       stylesheet_link_tag(user_agent_name.to_s, options)
